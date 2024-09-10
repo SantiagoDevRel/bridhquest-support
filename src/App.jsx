96e239287266463132765ce0dad66e24982aeb9d
santiagodevrel <santiago@chainsafe.io>
2024-09-10 16:08:28 +0300

    removed web3zksyncl2

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 import { Web3 } from "web3";
-import { ZKsyncPlugin, Web3ZKsyncL2 } from "web3-plugin-zksync";
+import { ZKsyncPlugin } from "web3-plugin-zksync";
 
 function App() {
   const [count, setCount] = useState(0);
 
   const web3 = new Web3();
 
-  const l2Provider = new Web3ZKsyncL2(window.ethereum);
-
-  web3.registerPlugin(new ZKsyncPlugin(l2Provider));
+  web3.registerPlugin(new ZKsyncPlugin(window.ethereum));
 
   async function sendOneWei() {
     const accounts = await web3.ZKsync.L2.eth.requestAccounts();
@@ -29,9 +27,9 @@ function App() {
     const populated = await web3.ZKsync.L2.populateTransaction(tx);
     console.log("populated", populated);
 
-    const signature = await web3.ZKsync.L2.eth.sendTransaction(populated);
+    const txReceipt = await web3.ZKsync.L2.eth.sendTransaction(populated);
 
-    console.log("signature", signature);
+    console.log("signature", txReceipt.transactionHash);
   }
 
   return (
