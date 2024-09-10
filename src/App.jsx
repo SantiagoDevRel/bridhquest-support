3ee705815a8a84279bbb807943cac149aa47ef74
santiagodevrel <santiago@chainsafe.io>
2024-09-10 15:17:00 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { Web3 } from "web3";
+import { ZKsyncPlugin, Web3ZKsyncL2 } from "web3-plugin-zksync";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const web3 = new Web3();
+
+  const l2Provider = new Web3ZKsyncL2(window.ethereum);
+
+  web3.registerPlugin(new ZKsyncPlugin(l2Provider));
+
+  async function sendOneWei() {
+    //const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+
+    const accounts = await web3.ZKsync.L2.eth.requestAccounts();
+    console.log("address", accounts[0]);
+
+    const to = web3.eth.accounts.create();
+
+    const tx = {
+      from: accounts[0],
+      to: to.address,
+      value: 1,
+    };
+
+    const populated = await web3.ZKsync.L2.populateTransaction(tx);
+    console.log("populated", populated);
+
+    const signature = await web3.ZKsync.L2.eth.accounts.signTransaction(populated);
+    //const signature = await web3.ZKsync.L2.signTransaction(populated);
+
+    console.log("signature", signature);
+  }
 
   return (
     <>
@@ -18,18 +50,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={sendOneWei}>Send 1 wei</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
